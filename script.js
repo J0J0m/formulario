9293a19350908fb58dc50718acadd7935321c464
J0J0m <joelma.pedrozo@escola.pr.gov.br>
2025-09-02 19:16:57 -0300

    .

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-        const palavras =  processatexto (texto) 
-}    campoResultado.textContent = palavras.join(", ");
+        const PalavrasChave =  processatexto (texto) 
+
+}    campoResultado.textContent = PalavrasChave .join(", ");
+
+
+function processaTexto(texto){
+ let palavras = Text.split (", ");
+ return palavras;  
+}
